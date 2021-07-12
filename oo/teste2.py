3b4674c206b64cc2e55728d81c5e362a44610c0a
Danilo Cruz <danilo.oliveiradacruz@gmail.com>
2021-07-12 18:00:35 -0300

    Wip - Python OO parte 2

---
+++ oo/teste2.py
@@ -0,0 +1,43 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+vingadores = Filme('vingadores - guerra infinita', 2019, 160)
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('Atlanta', 2018, 3)
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Duração: {atlanta.temporadas} - Likes: {atlanta.likes}')
